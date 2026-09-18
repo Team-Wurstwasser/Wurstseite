d2d95740b0ed87239696f22d6ea58936c7e831d6
hallo123wert <79359578+hallo123wert@users.noreply.github.com>
2026-09-18 12:10:59 +0200

    Redirect to matheliebsch.html on game over (#115)

--- js/games/matheliebsch.js
+++ js/games/matheliebsch.js
@@ -252,7 +252,7 @@ function startCounter(){
             player.size=100;
             clearInterval(counterInterval);
             alert("Zeit überlebt: "+gameState.counter+" Sekunden");
-            window.location.href="index.html";
+            window.location.href="games/matheliebsch.html";
         }
     },1000);
 
